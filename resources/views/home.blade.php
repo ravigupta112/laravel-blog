092fb20c25aba49c94a956add56e21a4c2f6681f
Ravi Gupta <ravigupta112>
2018-10-18 16:02:19 +0530

    Added Login register using
     php artisan make:auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Welcome to your login area!!
                 </div>
             </div>
         </div>
